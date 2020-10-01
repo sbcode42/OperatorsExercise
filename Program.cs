9e61663cd02f5a6d71b27813d2df1a196ed2159f
Sam Booth <ballersabooth@yahoo.com>
2020-10-01 15:44:17 -0500

    started excercise

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperatorExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
